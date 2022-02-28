d8bbd9651c078fcae90402a8609c2e2e5b2f734e
IT21289002 <99586424+IT21289002@users.noreply.github.com>
2022-02-28 06:02:33 +0000

    all done

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
@@ -8,5 +9,8 @@ int main() {
      cout <<  r+1 
           <<  names[r]
           << marks[r] << endl;
+   for (int r=0;r<5;r++){
+     cout<<setw(5)<<r+1<<setw(15)<<names[r]<<setw(10)<<setiosflags(ios::fixed)<<setprecision(2)<<marks[r]<<endl;
+     }
  }
 }
